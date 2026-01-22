c6e55b82434468b5e67f089abd4b2cf3798b1ab0
j4lib <julia.beck@lir.gu.se>
2026-01-22 17:13:07 +0100

    Fix chosen js and use the one from main application.

--- asset/js/chosen-init.js
+++ asset/js/chosen-init.js
@@ -1,11 +1,25 @@
-(function () {
-    function initChosen() {
-      if (typeof jQuery === 'undefined' || !jQuery.fn || !jQuery.fn.chosen) return;
-      jQuery('#search-facets select.chosen-select').chosen({
-        width: '100%',
-        search_contains: true
-      });
-    }
+jQuery(function ($) {
+    // Use chosenOptions from main
+    var opts = window.chosenOptions || { width: '100%', search_contains: true };
   
-    document.addEventListener('DOMContentLoaded', initChosen);
-  })();
+    $('.chosen-select').each(function () {
+      var $this = $(this);
+      // don't initialize twice
+      if ($this.data('chosen')) return;
+      $this.chosen(opts);
+    });
+  
+    $(document).on('chosen:showing_dropdown', '.chosen-select', function (e) {
+      var chosenContainer = $(e.target).next('.chosen-container');
+      var dropdown = chosenContainer.find('.chosen-drop');
+      var dropdownTop = dropdown.offset().top - $(window).scrollTop();
+      var dropdownHeight = dropdown.height();
+      var viewportHeight = $(window).height();
+      if (dropdownTop + dropdownHeight > viewportHeight) {
+        chosenContainer.addClass('chosen-drop-up');
+      }
+    });
+    $(document).on('chosen:hiding_dropdown', '.chosen-select', function (e) {
+      $(e.target).next('.chosen-container').removeClass('chosen-drop-up');
+    });
+  });
